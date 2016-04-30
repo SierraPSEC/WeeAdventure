9edab8d6b67fba0806e976520988707edc85656a
Cameron Eggleston <ceggleston55@gmail.com>
2016-04-30 16:56:07 -0700

    modified indentation

--- CE_python_files/CE_01.py
+++ CE_python_files/CE_01.py
@@ -1,17 +1,13 @@
-
-
-
-
 f = open('CE_WeeAdventure01.txt')
 line = f.readline()
 
 while line:
-	if "Scenario" in line:
-	   line = f.readline()
-	   while line:
-		if "ScenarioEnd" in line:
-		 break
-		print(line.strip())
-		line = f.readline()
-	line = f.readline()
+  if "Scenario" in line:
+    line = f.readline()
+    while line:
+      if "ScenarioEnd" in line:
+        break
+      print(line.strip())
+      line = f.readline()
+  line = f.readline()
 f.close()
